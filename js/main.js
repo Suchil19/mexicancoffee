9c8bcdaea1d3a2e4ca88031c4c0ee2a9b6489dba
suchil19 <jlsuchil@gmail.com>
2024-10-18 01:02:54 -0600

    12

--- js/main.js
+++ js/main.js
@@ -54,3 +54,4 @@ var bubblyButtons = document.getElementsByClassName("bubbly-button");
 for (var i = 0; i < bubblyButtons.length; i++) {
   bubblyButtons[i].addEventListener('click', animateButton, false);
 }
+
